17ee77b41416778d3c90466990c1311272caa8a6
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2025-03-23 04:03:59 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github-pages';
+
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  adapter: github(),
   base: '/portfolio/', // ⚠️ IMPORTANTE: cambia esto por el nombre de tu repositorio
   vite: {
     plugins: [tailwindcss()]
